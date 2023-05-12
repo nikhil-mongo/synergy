78099037526013f87b2a306431c6d07e90489ca6
Nikhil Singh <nikhil.singh@mongodb.com>
2023-05-12 17:32:58 +0530

    add aws

--- data.tf
+++ data.tf
@@ -1,14 +1,14 @@
-data "aws_vpc" "main" {
-  id = var.aws_vpc_id
-}
+# data "aws_vpc" "main" {
+#   id = var.aws_vpc_id
+# }
 
-data "aws_subnets" "vpc_all" {
-  filter {
-    name   = "vpc-id"
-    values = [var.aws_vpc_id]
-  }
-}
+# data "aws_subnets" "vpc_all" {
+#   filter {
+#     name   = "vpc-id"
+#     values = [var.aws_vpc_id]
+#   }
+# }
 
-data "vault_generic_secret" "users_secret" {
-  path = "secrets/mongodb/users/${var.environment}"
-}
+# data "vault_generic_secret" "users_secret" {
+#   path = "secrets/mongodb/users/${var.environment}"
+# }
